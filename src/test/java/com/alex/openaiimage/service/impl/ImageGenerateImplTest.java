0b399984116d99f74523158b1c77461f4c9469c5
alex <907785012@qq.com>
2022-12-08 16:51:24 +0800

    Modify response status and move file for maintain standard

--- src/test/java/com/alex/openaiimage/service/impl/ImageGenerateImplTest.java
+++ src/test/java/com/alex/openaiimage/service/impl/ImageGenerateImplTest.java
@@ -1,15 +1,13 @@
 package com.alex.openaiimage.service.impl;
 
 import com.alex.openaiimage.service.ImageGenerate;
-import com.alex.openaiimage.service.ImageSize;
+import com.alex.openaiimage.entity.ImageSize;
 import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.io.IOException;
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class ImageGenerateImplTest {
